7451abdf43489058a6fbb77877df78db8c977bec
bstyx <sebastianludtke@protonmail.com>
2019-12-23 09:16:15 +0100

    Updates

---
+++ precache-manifest.e8f4f8483151de6fccbf60db37836538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e66ff4ad76edbe97bb5acea4d62110",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "c8871d2509dddbcf6653",
+    "url": "/todoapp/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "17bbc28eef42d6db6653",
+    "url": "/todoapp/static/js/2.be359989.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/todoapp/static/js/2.be359989.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8871d2509dddbcf6653",
+    "url": "/todoapp/static/js/main.c6cb8c82.chunk.js"
+  },
+  {
+    "revision": "92cb198941b632a981d3",
+    "url": "/todoapp/static/js/runtime-main.42bad35b.js"
+  }
+]);
